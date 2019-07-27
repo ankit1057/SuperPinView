58d2b29fb94707dcbee85e97f2b0471e19c89a42
ankit <projectsbyankit@gmail.com>
2019-07-27 12:56:23 +0530

    Initial commit

---
+++ app/src/main/java/io/ankit/superpinview/MainActivity.java
@@ -0,0 +1,14 @@
+package io.ankit.superpinview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
